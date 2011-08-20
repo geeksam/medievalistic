10fd542db6bbfa7570aba226783998712cc0661e
Sam Livingston-Gray <geeksam@gmail.com>
2011-08-20 01:17:57 -0700

    make router pass tests

--- lib/medievalistic/router.rb
+++ lib/medievalistic/router.rb
@@ -7,32 +7,39 @@ module Medievalistic
       request  = Rack::Request.new(env)
       response = Rack::Response.new
 
-      controller_name, action_name, *_ = path_components(request.path_info)
-      begin
-        controller_class = load_controller_class(controller_name)
-      rescue RoutingError => e
-        raise e.class.new(e.message + " (path: '#{request.path_info}')")
-      end
+      controller_class, action = route(request.path_info)
+
+      controller = controller_class.new(request, response)
+      controller.send(action)
 
       response.finish
     end
 
+    def route(path)
+      controller_name, action_name, *_ = path_components(path)
+      controller_class = load_controller_class(controller_name, path)
+      [controller_class, action_name]
+    end
 
     protected
 
     def path_components(path_info)
-      path_info.gsub(/^\//, '').split('/')
+      components = path_info.gsub(/^\//, '').split('/')
+      case components.length
+      when 1 then components << 'index'
+      end
+      components
     end
 
     def controller_class_name(controller_name)
       controller_name.gsub(/^(.)/) { |m| m[0].chr.upcase } + 'Controller'
     end
 
-    def load_controller_class(controller_name)
+    def load_controller_class(controller_name, path)
       klass_name = controller_name.gsub(/^(.)/) { |m| m[0].chr.upcase } + 'Controller'
       Object.const_get(klass_name)
     rescue NameError
-      raise RoutingError.new("Unknown controller class: #{klass_name}")
+      raise RoutingError.new("Unknown controller class: #{klass_name} (path: '#{path}')")
     end
   end
 end
