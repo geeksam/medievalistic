63e698e4f0fb39dfda13bcc9227b2cb199497ed1
Sam Livingston-Gray <geeksam@gmail.com>
2011-08-20 15:46:29 -0700

    Tweak test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,15 @@
 require 'rubygems'
 require 'bundler'
 require 'minitest/autorun'
-require File.join(File.dirname(__FILE__), *%w[.. lib medievalistic])
+
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. lib medievalistic]))
+
+
+Hello = 'Hello, world!'
+def Hello.txt
+  self.dup
+end
+def Hello.html
+  '<p>%s</p>' % self
+end
+Hello.freeze
