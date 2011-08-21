d8106e6269383008559198aa3a925c8551d6fe91
Sam Livingston-Gray <geeksam@gmail.com>
2011-08-20 23:22:52 -0700

    Refactor controller and its unit tests

--- lib/medievalistic/controller.rb
+++ lib/medievalistic/controller.rb
@@ -23,20 +23,25 @@ module Medievalistic
 
     def render(*args)
       options = args.extract_options!
-      ContentTypes.keys.each do |type|
-        try_rendering(options, type)
+      case
+      when (ContentTypes.keys & options.keys).present?
+        try_rendering_format_value(options)
       end
     end
 
     protected
-    def try_rendering(options, type)
-      return unless options.has_key?(type)
+    def try_rendering_format_value(options)
+      (ContentTypes.keys & options.keys).each do |type|
+        next unless options.has_key?(type)
+        content = options.delete(type)
+        write_type_and_content(ContentTypes[type], content)
+      end
+    end
+
+    def write_type_and_content(content_type, content)
       raise DoubleRenderError if @rendered
-
       @rendered = true
-
-      response["Content-Type"] = ContentTypes[type]
-      content = options.delete(type)
+      response["Content-Type"] = content_type
       response.write content
     end
   end
--- test/unit/controller_unit_test.rb
+++ test/unit/controller_unit_test.rb
@@ -16,46 +16,6 @@ describe Medievalistic::Controller do
       end
     end
 
-    it 'renders :text => nil and sets content type to text/plain' do
-      def @controller.goodbye
-        render :text => nil
-      end
-
-      @controller.response.expect_content_and_type nil, 'text/plain' do
-        @controller.send(:goodbye)
-      end
-    end
-
-    it 'renders :text => "" and sets content type to text/plain' do
-      def @controller.goodbye
-        render :text => ''
-      end
-
-      @controller.response.expect_content_and_type '', 'text/plain' do
-        @controller.send(:goodbye)
-      end
-    end
-
-    it 'renders :text => "foo" and sets content type to text/plain' do
-      def @controller.goodbye
-        render :text => Hello.txt
-      end
-
-      @controller.response.expect_content_and_type Hello.txt, 'text/plain' do
-        @controller.send(:goodbye)
-      end
-    end
-
-    it 'renders :html => "foo" and sets content type to text/html' do
-      def @controller.goodbye
-        render :html => Hello.html
-      end
-
-      @controller.response.expect_content_and_type Hello.html, 'text/html' do
-        @controller.send(:goodbye)
-      end
-    end
-
     it 'raises a DoubleRenderError when you call render more than once' do
       def @controller.goodbye
         render :html => 'Unce'
@@ -65,20 +25,68 @@ describe Medievalistic::Controller do
 
       assert_raises(Medievalistic::Controller::DoubleRenderError) do
         @controller.response.expect_content_and_type 'Unce', 'text/html' do
-          @controller.send(:goodbye)
+          @controller.goodbye
         end
       end
     end
 
-    it 'raises a DoubleRenderError when you call render with more than one format key' do
-      def @controller.goodbye
-        render :text => 'I am text', :html => 'I am HTML'
+    describe 'with only hash-style arguments' do
+      it 'renders :text => nil and sets content type to text/plain' do
+        def @controller.goodbye
+          render :text => nil
+        end
+
+        @controller.response.expect_content_and_type nil, 'text/plain' do
+          @controller.goodbye
+        end
       end
 
-      assert_raises(Medievalistic::Controller::DoubleRenderError) do
-        @controller.response.expect_content_and_type 'I am HTML', 'text/html' do
-          @controller.send(:goodbye)
+      it 'renders :text => "" and sets content type to text/plain' do
+        def @controller.goodbye
+          render :text => ''
         end
+
+        @controller.response.expect_content_and_type '', 'text/plain' do
+          @controller.goodbye
+        end
+      end
+
+      it 'renders :text => "foo" and sets content type to text/plain' do
+        def @controller.goodbye
+          render :text => Hello.txt
+        end
+
+        @controller.response.expect_content_and_type Hello.txt, 'text/plain' do
+          @controller.goodbye
+        end
+      end
+
+      it 'renders :html => "foo" and sets content type to text/html' do
+        def @controller.goodbye
+          render :html => Hello.html
+        end
+
+        @controller.response.expect_content_and_type Hello.html, 'text/html' do
+          @controller.goodbye
+        end
+      end
+
+      it 'raises a DoubleRenderError when you call render with more than one format key' do
+        def @controller.goodbye
+          render :text => 'I am text', :html => 'I am HTML'
+        end
+
+        assert_raises(Medievalistic::Controller::DoubleRenderError) do
+          @controller.response.expect_content_and_type 'I am HTML', 'text/html' do
+            @controller.goodbye
+          end
+        end
+      end
+    end
+
+    describe 'with no arguments' do
+      it 'renders a template' do
+        flunk "Continue here"
       end
     end
   end
