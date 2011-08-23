bbba66314c9bf409d2ca924d4ad20ea92b60d9cd
Sam Livingston-Gray <geeksam@gmail.com>
2011-08-22 21:13:31 -0700

    ENCAPSULAAAATE!

--- lib/medievalistic/doublemeat_medley.rb
+++ lib/medievalistic/doublemeat_medley.rb
@@ -2,8 +2,6 @@ require 'forwardable'
 
 module Medievalistic
   class DoublemeatMedley
-    attr_reader :app, :rack_request, :rack_response
-    
     def initialize(app, env)
       @app = app
       @rack_request  = Rack::Request.new(env)
@@ -11,12 +9,12 @@ module Medievalistic
     end
 
     extend Forwardable
-    def_delegator :rack_request, :path_info, :path
-    def_delegator :rack_response, :finish, :finalize
+    def_delegator :@rack_request, :path_info, :path
+    def_delegator :@rack_response, :finish, :finalize
 
     def write_type_and_content(content_type, content)
-      rack_response["Content-Type"] = content_type
-      rack_response.write content
+      @rack_response["Content-Type"] = content_type
+      @rack_response.write content
     end
   end
 end
